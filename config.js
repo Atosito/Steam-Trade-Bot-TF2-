9118a6ec7fbcaa4bfad46a97a6566ded0b88e36c
Nyjako <31698318+Nyjako@users.noreply.github.com>
2018-03-09 04:11:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,13 @@ module.exports = {
         identity_secret: '',
         id_bossAccount: '76561198050753995',
 
+        weapons: {
+            buy: true, // Allow to pay in weapons
+            sell: true, // Allow to buy weapons
+            buyWeaponsToScrap: 2, // How many weapons you give for one scrap
+            sellWeaponsToScrap: 2 // How many weapons you take for one scrap
+        },
+        
 
 //WIP - Ignore This -
         tradeAccepted : '+rep Thanks for using our service. Enjoy your items =). Wall-E',
