8cc458b4868322d5549d7f4897466f2a2d88b54b
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2018-03-22 20:21:41 +0100

    Eh
    Could just do this :P
    Might be a good idea to make a package.json file with all dependencies and change this file to only "npm i"

--- Install.bat
+++ Install.bat
@@ -1,1 +1,1 @@
-npm install tf2 && npm install steam-user && npm install steam-totp && npm install steamcommunity && npm install steam-tradeoffer-manager && npm install colors
+npm install tf2 steam-user steam-totp steamcommunity steam-tradeoffer-manager colors
