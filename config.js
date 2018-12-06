6507736017778bf50cef5beb63e55b67a76e2352
Tomas <gonzaltomas@gmail.com>
2018-12-06 14:06:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,10 @@
 module.exports = {
-
         accountName: '',
         password: '',
         shared_secret: '',
         botName: '[BOT] Wall-E Beta (TF2)',
-        identity_secret: '',
-        id_bossAccount: '76561198050753995',
-
-
-//WIP - Ignore This -
-        tradeAccepted : '+rep Thanks for using our service. Enjoy your items =). Wall-E',
-        tradeDeclined : 'Ops.. We are sorry, we had to decline your trade. Item overstocked or you put low value. Wall-E',
-        tradeInvalid : 'Ops.. We detected that some of the items are no longer available. Re-send Trade Offer. Wall-E ',
-        tradeCanceled : 'You have canceled our trade. Nevermind, hope to see you again on ours log. Wall-E',
-
-
-  
+        identitySecret: '',
+        arr_idBossAccount: ["76561198050753995"],
+        accept_gifted: true,
+        trash_defindex: [5875,5893]
   };
