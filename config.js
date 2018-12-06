28656a28e4bb6c1fe110e1c59adcf5d7d45bd270
Tomas <gonzaltomas@gmail.com>
2018-12-06 14:17:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ module.exports = {
         shared_secret: '',
         botName: '[BOT] Wall-E Beta (TF2)',
         identitySecret: '',
-        arr_idBossAccount: ["76561198050753995"],
-        accept_gifted: true,
-        trash_defindex: [5875,5893]
+        arr_idBossAccount: ["76561198050753995"], // array of steamID64 that bot will accept offer 
+        accept_gifted: true, // true for accept gifted items - false for not accept them
+        trash_defindex: [5875,5893], //Defindex of items that bot will delete
+        min_Reclaimeds: 2, // Number of reclaimeds that always need to exists in backpack
+        min_Scraps:2,// Number of scraps that always need to exists in backpack
   };
